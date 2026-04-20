55342ef343e20ca1ba0f5428eda6d6cbe2c1dfbf
Nassib Aboud <132102065+Nassibab@users.noreply.github.com>
2026-04-20 13:18:53 +0200

    Configure Chrome options for headless execution
    Added headless mode and performance options for Chrome.

--- WG-Gesucht.py
+++ WG-Gesucht.py
@@ -21,6 +21,10 @@ chrome_options.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome(options=chrome_options)
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920,1080")
 
 
 #driver.quit()
