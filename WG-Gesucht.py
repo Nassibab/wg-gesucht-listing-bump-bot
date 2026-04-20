c71cc8d88c52ada5b7411f441b712f18340e5796
Nassib Aboud <132102065+Nassibab@users.noreply.github.com>
2026-04-20 13:22:59 +0200

    Refactor Chrome driver initialization in WG-Gesucht.py

--- WG-Gesucht.py
+++ WG-Gesucht.py
@@ -18,13 +18,13 @@ password = os.getenv("WG_GESUCHT_PASSWORD")
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-
-
-driver = webdriver.Chrome(options=chrome_options)
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
+
+
+driver = webdriver.Chrome(options=chrome_options)
 
 
 #driver.quit()
